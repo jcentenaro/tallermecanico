7bb79c2387f67f6445acdc08da4124dabd6585dd
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2025-04-18 13:07:33 -0300

    cambios en index por mainRoutes

--- index.js
+++ index.js
@@ -2,45 +2,32 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
-const bodyParser = require("body-parser");
-const mainRoutes = require(path.join(__dirname, "/src/routes/mainRoutes"));
-const contactRoutes = require("./src/routes/mainRoutes");
-
 const methodOverride = require("method-override");
 
-// Middleware para procesar formularios
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// Usar las rutas de contacto
-app.use("/contacto", mainRoutes);
-// habilito vista carpetat public
-app.use(express.static(path.join(__dirname, "/public")));
-// configuración del motor de vistas
-app.set("view engine", "ejs");
-//y le digo en carpeta están las vistats
-app.set("views", path.join(__dirname, "/src/views"));
-// LE DIGO QUE UTILICE LAYOUTS Y EN DONDE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(expressLayouts);
-app.set("layout", "layouts/layout");
-// le pido a la app que use la constatnte que cargué arriba
 app.use(methodOverride("_method"));
 
-app.use(express.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+app.set("layout", "layouts/layout");
 
+const mainRoutes = require("./src/routes/mainRoutes");
+app.use("/contacto", mainRoutes); // Si mainRoutes incluye rutas para /contacto
+app.use(mainRoutes); // Rutas principales
 
-app.use(mainRoutes);
-
-// Manejo de error de página no existente. 
-app.use((req, res, next) => {
-    // res.status(404).send("La página que está buscando no existe");
-    res.status(404).render("404", { 
-        layout: "layouts/layout", 
-        titulo: "Error 404",
-        currentRoute: "/testimonial",
-        title: "Página no encontrada",
-        ciudad: "CABA", 
-        zona: "Error 404" });
+app.use((req, res) => {
+  res.status(404).render("404", {
+    layout: "layouts/layout",
+    titulo: "Error 404",
+    currentRoute: "/testimonial",
+    title: "Página no encontrada",
+    ciudad: "CABA",
+    zona: "Error 404",
+  });
 });
 
-const PORT = 3000
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
